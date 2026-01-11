bb6ba3a954b39ea2f1955b393cd4bc586ae2709b
thinico <85040813+TheThinico@users.noreply.github.com>
2026-01-11 17:42:02 -0300

    Conecção com Banco de Dado
    Codigo do Banco de dados Funcionando perfeitamente
    A fazer: cruzamento do código do banco com o menu

--- main_test.py
+++ main_test.py
@@ -1,8 +1,13 @@
-import database_connect
+import banco_de_dados
 
 # user_name = "thiago"
 # pass_word = "mPKueWWlKg2MsWhk"
 user_name = (input("NOME: "))
 pass_word = (input("SENHA: "))
-database_connect.set_login_datas(user_name, pass_word)
-database_connect.database_login()
+
+#Codigo pra linkar ao banco o usuario
+data = banco_de_dados.conectar_banco(user_name, pass_word)
+
+#database_connect.link_database(user_name, pass_word)
+#database_connect.database_login()
+
